951edf5355716015df62e25ee446401d9cd76f51
d1Rocky <edenyovan@gmail.com>
2025-05-17 14:01:33 -0500

    Quick nmap scan will be initiated to find all open ports. Nmap scan will check specific vulnerable open ports by extracting them from the first scan

--- recon_scan.sh
+++ recon_scan.sh
@@ -10,3 +10,19 @@ if [ $? -eq 0 ]; then
 else
     echo "[!] $target is not reachable. Exiting."
     exit 1
+
+fi
+
+echo "[*] Running quick Nmap scan on all ports..."
+nmap_output=$(nmap -p- -Pn -T5 -v $target)
+
+# Ports of interest
+ports_to_check=(3389 22 23 445 80 8080 21 25)
+open_ports=()
+
+# Extract open ports from the scan
+for port in "${ports_to_check[@]}"; do
+    if echo "$nmap_output" | grep -q "$port/tcp open"; then
+        open_ports+=("$port")
+    fi
+done
