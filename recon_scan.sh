24c64f1530113db696c4dc33be1425cc1c816e49
d1Rocky <edenyovan@gmail.com>
2025-05-17 13:59:30 -0500

    If ping is reachable the scan will continue, otherwise scan will stop and exit.

--- recon_scan.sh
+++ recon_scan.sh
@@ -5,4 +5,8 @@ read -p "Enter target IP or domain: " target
 echo "[*] Pinging $target..."
 ping -c 3 $target > /dev/null
 
-
+if [ $? -eq 0 ]; then
+    echo "[*] $target is reachable. Continuing scan..."
+else
+    echo "[!] $target is not reachable. Exiting."
+    exit 1
